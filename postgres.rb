80ae0b3c52452225f2aa3a35b24751bd5af95960
vivek0293 <vivek0293@outlook.com>
2018-11-01 12:04:34 -0700

    Update postgres.rb

--- postgres.rb
+++ postgres.rb
@@ -4,7 +4,7 @@ require 'csv'
 begin
 
     con = PG.connect :host => 'ec2-54-83-202-218.compute-1.amazonaws.com', :dbname => 'd6slr4o1nr2vnj', :user => 'ykipxjtxndkxpl', 
-        :password => 'B0PBOlfJtm_DPf3j-h4F3DrT0r'
+        :password => '********'
 
     puts "Connected" 
 
